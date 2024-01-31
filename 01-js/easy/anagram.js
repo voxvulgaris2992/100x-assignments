96cdf3c2ff251eff83f709f2281c4e1c5d1c7d19
Raj Singh <singh4991raj@gmail.com>
2024-01-31 16:48:56 +0000

    passed

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,31 @@
 */
 
 function isAnagram(str1, str2) {
-
+  const strings = [str1.toLowerCase(), str2.toLowerCase()];
+  let objects = [];
+  function sortObjectByKey(object) {
+    const sortedKeys = Object.keys(object).sort();
+    const sortedObject = {};
+    for (const key of sortedKeys) {
+      sortedObject[key] = object[key];
+    }
+    return sortedObject;
+  }
+  for (const string of strings) {
+    charCount = {};
+    for (const char of string) {
+      charCount[char] = (charCount[char] || 0) + 1;
+    }
+    objects.push(charCount);
+  }
+  if (JSON.stringify(sortObjectByKey(objects[0])) === JSON.stringify(sortObjectByKey(objects[1]))) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
 module.exports = isAnagram;
+
+// console.log(main("adc!", "!bac"));
